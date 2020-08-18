fa126d414c02d935b76b63cdc9b93f24aebb8a3f
Thomson <thomsonbingo@gmail.com>
2020-08-18 10:31:59 +0530

    Revision version 3

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import Logo from '../../public/images/logo-2.png';
+
 
 const Header = () => {
     return (
         <header>
+            <img src={Logo} id="header-logo"/>
+            <div id="navlinks">
             <h4>Home</h4>
             <h4>Services</h4>
             <h4>Contact us</h4>
+            </div>
         </header>
     )
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import Logo from '../../public/images/logo-1.png';
 
-const Home = () => {
+class Home extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount() {
+
+        setTimeout(()=>{
+            document.getElementById('roll-text').textContent = "Web dev";
+        },3000)
+        setTimeout(()=>{
+            document.getElementById('roll-text').textContent = "Digi Mark";
+        },3000)
+        
+    }
+
+    render() {
     return(
         <div id="home">
             <div className="background"></div>
             <img src={Logo} id="logo"></img>
-            <p>Digital marketing, web development and much more...</p>
+            <p id="roll-text">Digital marketing</p>
             <h3 id="services-header">SERVICES</h3>
         </div>
-    )
+    )}
 }
 
 export default Home;
